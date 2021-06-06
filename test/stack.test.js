d45c2e36774a242154f943e492b8281f4b8b4789
Chad Bazil Bosch <chadbosch08@gmail.com>
2021-06-06 11:11:45 +0200

    fireship tutorial

---
+++ test/stack.test.js
@@ -0,0 +1,44 @@
+class Stack {
+  constructor() {
+    this.top = -1,
+    this.items = {}
+  }
+  get peek() {
+    return this.items[this.top];
+  }
+
+  popOff() {
+    delete this.items[this.top]
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value
+  }
+}
+describe('My Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it('can push to the top', () => {
+    stack.push('🥑')
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('🥑');
+  });
+
+  it('can pop off', () => {
+    stack.push('🥑')
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('🥑');
+    stack.popOff();
+    expect(stack.items).toEqual({});
+  });
+})
